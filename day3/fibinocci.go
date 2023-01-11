0bf5247988b0f370511f8905d3ec410a19e542d3
Sachin <sachin.ms@zopsmart.com>
2023-01-11 14:38:33 +0530

    Fibinocci series

---
+++ day3/fibinocci.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibinocci(n int) {
+	var n3, n2, n1 int = 0, 0, 1
+
+	for i := 1; i <= n; i++ {
+		fmt.Println(n3)
+		n3 = n1 + n2
+		n1 = n2
+		n2 = n3
+	}
+}
+
+func main() {
+	//fibinocci(5)
+	fibinocci(10)
+}
