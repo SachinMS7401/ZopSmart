1756cfef4e4b4c9982967ad7d46974b6d98aa641
Sachin <sachin.ms@zopsmart.com>
2023-01-11 18:18:10 +0530

    Basic Programs

--- day3/Prime_number.go
+++ day3/Prime_number.go
@@ -1,13 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func checkPrime(n int){
-	if n<2{
-		fmt.Prinln("number must be greater than 2")
+func checkPrime(n int) {
+	if n < 2 {
+		fmt.Println("number must be greater than 2")
+		return
 	}
-	n
-	for i:=2;i<=
+	var n3 int = int(math.Sqrt(float64(n)))
+	for i := 2; i <= n3; i++ {
+		if n%i == 0 {
+			fmt.Printf("%v is not a prime number", n)
+			return
+		}
+	}
+	fmt.Println("n is a prime number")
+	return
+
 }
 
 func main() {
